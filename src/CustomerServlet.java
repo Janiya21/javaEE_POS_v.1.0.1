04ff92aa367cdf85af118f9dcd8be80d653db2fc
JanithSandaru <janithsandaru999@gmail.com>
2022-05-10 13:29:16 +0530

    added

--- src/CustomerServlet.java
+++ src/CustomerServlet.java
@@ -1,3 +1,43 @@
-public class CustomerServlet {
+import javax.annotation.Resource;
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+
+@WebServlet(urlPatterns = "/customer")
+public class CustomerServlet extends HttpServlet {
+
+    @Resource(name = "java:comp/env/jdbc/pool")
+    DataSource dataSource;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            resp.setContentType("application/json");
+            Connection connection = dataSource.getConnection();
+
+            PrintWriter writer = resp.getWriter();
+
+            ResultSet rst = connection.prepareStatement("select * from Customer").executeQuery();
+            while (rst.next()){
+                System.out.println(rst.getString(1) + " id");
+            }
+            connection.close();
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+    }
 }
