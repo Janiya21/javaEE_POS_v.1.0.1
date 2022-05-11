0eb1b637e078b880207111f3dea81f06645f0f5d
JanithSandaru <janithsandaru999@gmail.com>
2022-05-11 09:42:21 +0530

    customer added

--- src/CustomerServlet.java
+++ src/CustomerServlet.java
@@ -23,19 +23,47 @@ public class CustomerServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        PrintWriter writer = resp.getWriter();
+
         try {
             resp.setContentType("application/json");
             Connection connection = dataSource.getConnection();
 
-            PrintWriter writer = resp.getWriter();
-
             ResultSet rst = connection.prepareStatement("select * from Customer").executeQuery();
+            JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+
             while (rst.next()){
-                System.out.println(rst.getString(1) + " id");
+                String id = rst.getString(1);
+                String name = rst.getString(2);
+                String email = rst.getString(3);
+                int tel = rst.getInt(4);
+
+                JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+
+                objectBuilder.add("id", id);
+                objectBuilder.add("name", name);
+                objectBuilder.add("email", email);
+                objectBuilder.add("telNo", tel);
+
+                arrayBuilder.add(objectBuilder.build());
             }
+
+            JsonObjectBuilder response = Json.createObjectBuilder();
+            response.add("status", 200);
+            response.add("message", "Done");
+            response.add("data", arrayBuilder.build());
+            writer.print(response.build());
+
             connection.close();
 
         } catch (SQLException throwables) {
+            JsonObjectBuilder response = Json.createObjectBuilder();
+            response.add("status", 400);
+            response.add("message", "Error");
+            response.add("data", throwables.getLocalizedMessage());
+            writer.print(response.build());
+            resp.setStatus(HttpServletResponse.SC_OK); //200
             throwables.printStackTrace();
         }
 
