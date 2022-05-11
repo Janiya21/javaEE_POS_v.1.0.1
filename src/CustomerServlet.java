25d040bf2848ce03e2e7cf71e7cc821b28c5f1d2
JanithSandaru <janithsandaru999@gmail.com>
2022-05-11 10:49:27 +0530

    customer added done

--- src/CustomerServlet.java
+++ src/CustomerServlet.java
@@ -11,6 +11,7 @@ import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -66,6 +67,45 @@ public class CustomerServlet extends HttpServlet {
             resp.setStatus(HttpServletResponse.SC_OK); //200
             throwables.printStackTrace();
         }
+    }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String customerId = req.getParameter("customerId");
+        String customerName = req.getParameter("customerName");
+        String customerEmail = req.getParameter("customerEmail");
+        String customerTelNo = req.getParameter("customerTelNo");
+
+        PrintWriter writer = resp.getWriter();
+
+        resp.setContentType("application/json");
+
+        try {
+            Connection connection = dataSource.getConnection();
+            PreparedStatement pstm = connection.prepareStatement("Insert into Customer values(?,?,?,?)");
+            pstm.setObject(1,customerId);
+            pstm.setObject(2,customerName);
+            pstm.setObject(3,customerEmail);
+            pstm.setObject(4,customerTelNo);
+
+            if(pstm.executeUpdate() > 0){
+                JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+                resp.setStatus(HttpServletResponse.SC_CREATED);
+                objectBuilder.add("status", 200);
+                objectBuilder.add("message", "Successfully Added");
+                objectBuilder.add("data", "");
+                writer.print(objectBuilder.build());
+            }
+            connection.close();
+
+        } catch (SQLException throwables) {
+            JsonObjectBuilder response = Json.createObjectBuilder();
+            response.add("status", 400);
+            response.add("message", "Error");
+            response.add("data", throwables.getLocalizedMessage());
+            writer.print(response.build());
+            resp.setStatus(HttpServletResponse.SC_OK);
+            throwables.printStackTrace();
+        }
     }
 }
