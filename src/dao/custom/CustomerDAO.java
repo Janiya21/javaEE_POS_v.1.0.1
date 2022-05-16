2764e7101945e51964451faa2483c8ce8f4db7f9
JanithSandaru <janithsandaru999@gmail.com>
2022-05-16 18:45:37 +0530

    added done'

--- src/dao/custom/CustomerDAO.java
+++ src/dao/custom/CustomerDAO.java
@@ -9,4 +9,7 @@ import java.util.List;
 
 public interface CustomerDAO extends SuperDAO {
     public List<Customer> getAllCustomers() throws SQLException;
+    public boolean addCustomer(Customer customer);
+    public boolean deleteCustomer(Customer customer);
+    public boolean updateCustomer(Customer customer);
 }
